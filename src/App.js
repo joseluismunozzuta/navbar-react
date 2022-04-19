246bdbebbf3ceb56f72dc59ed7607f5e5ae197e3
joseluismunozzuta <joseluismunozzuta@gmail.com>
2022-04-19 00:18:22 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from "./Navigation/Navbar.js";
+import "./bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return <>
+    <Navbar />
+    <h2>Las ofertas de la semana</h2>
+  </>;
+}
+
+
+export default App;
